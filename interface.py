b53ca144c0b9278685c79b9fb17d3273f7dce76d
chris_z <cristina.zappullo@gmail.com>
2022-05-24 21:30:48 +0100

    moved to supermarket

--- interface.py
+++ interface.py
@@ -8,10 +8,10 @@ name_length = len(name)
 st.write("Your name has ",name_length, "characters")
 
 # How to display data
-link = "https://raw.githubusercontent.com/chriszapp/datasets/main/books.csv"
-books = pd.read_csv(link)
-st.write(books)
+link = "https://raw.githubusercontent.com/chriszapp/datasets/main/supermarket_sales%20-%20Sheet1.csv"
+supermarket = pd.read_csv(link, header = 0)
+st.write(supermarket)
 
 # How to display graphics
-st.line_chart(books['ratings_count'])
+st.line_chart(supermarket['Quantity'])
 
