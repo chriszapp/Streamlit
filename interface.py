52f50a4838a6c658d7aa0e3f9dc970dfd9acba08
chris_z <cristina.zappullo@gmail.com>
2022-05-24 22:08:42 +0100

    before finance

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
 import streamlit as st
@@ -8,6 +9,7 @@ import streamlit as st
 nav_list = ["Total Earnings", "Locations", "Check the Data"]
 link = "https://raw.githubusercontent.com/chriszapp/datasets/main/supermarket_sales%20-%20Sheet1.csv"
 supermarket = pd.read_csv(link, header = 0)
+pivot = pd.pivot_table(supermarket, index = 'Date', values = 'Total', aggfunc = np.sum)#.sort_values('Date', inplace = True)
 
 with st.sidebar: 
     selection = st.radio(
@@ -16,18 +18,27 @@ with st.sidebar:
 
 if selection == nav_list[0]:
     st.title("Total Earnings")
+    # st.write(pivot)#[pivot['Branch'] == 'A'])
+    st.line_chart(data=pivot, use_container_width=True)
 
 
 if selection == nav_list[1]:
     st.title('We Are Here!')
-    name = st.text_input("Please give me your name:")
-    name_length = len(name)
-    st.write("Your name has ",name_length, "characters")
+    name_branch = st.text_input("Please select the branch (A, B, or C):")
+    if name_branch:
+        st.write("The branch ", name_branch, " is in ", supermarket[supermarket['Branch'] == name_branch]['City'].unique()[0])
 
 
 if selection == nav_list[2]:
     # How to display data
     st.write(supermarket)
+
+    # import yfinance as yf
+
+    # data = yf.download('AAPL','2016-01-01','2019-08-01')
+
+    # # get stock info
+    # st.write(data)
 
 
 
